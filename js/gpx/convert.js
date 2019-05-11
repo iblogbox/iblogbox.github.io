1c475c0b107492941a7cd9e0b3b7d9a40dbf151b
kcs <iblogbox@gmail.com>
2019-05-11 12:15:52 +0900

    Initial commit

--- js/gpx/convert.js
+++ js/gpx/convert.js
@@ -624,3 +624,9 @@ b)}}function l(a,b){b=!!b;a[0]=f(a[0],!b);for(var c=1;c<a.length;c++)a[c]=f(a[c]
 n,d){n.exports=[{key:"building",polygon:"all"},{key:"highway",polygon:"whitelist",values:["services","rest_area","escape","elevator"]},{key:"natural",polygon:"blacklist",values:["coastline","cliff","ridge","arete","tree_row"]},{key:"landuse",polygon:"all"},{key:"waterway",polygon:"whitelist",values:["riverbank","dock","boatyard","dam"]},{key:"amenity",polygon:"all"},{key:"leisure",polygon:"all"},{key:"barrier",polygon:"whitelist",values:"city_wall ditch hedge retaining_wall wall spikes".split(" ")},
 {key:"railway",polygon:"whitelist",values:["station","turntable","roundhouse","platform"]},{key:"area",polygon:"all"},{key:"boundary",polygon:"all"},{key:"man_made",polygon:"blacklist",values:["cutline","embankment","pipeline"]},{key:"power",polygon:"whitelist",values:["plant","substation","generator","transformer"]},{key:"place",polygon:"all"},{key:"shop",polygon:"all"},{key:"aeroway",polygon:"blacklist",values:["taxiway"]},{key:"tourism",polygon:"all"},{key:"historic",polygon:"all"},{key:"public_transport",
 polygon:"all"},{key:"office",polygon:"all"},{key:"building:part",polygon:"all"},{key:"military",polygon:"all"},{key:"ruins",polygon:"all"},{key:"area:highway",polygon:"all"},{key:"craft",polygon:"all"},{key:"golf",polygon:"all"}]},{}]},{},[1])(1)});
+
+if (typeof ArrayBuffer !== "undefined" && !ArrayBuffer['isView']) {
+  ArrayBuffer.isView = function(a) {
+    return a !== null && typeof(a) === "object" && a['buffer'] instanceof ArrayBuffer;
+  };
+}
