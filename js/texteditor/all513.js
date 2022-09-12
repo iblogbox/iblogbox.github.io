c7f877661efacb32914792a2c15203295d3de782
kcs <iblogbox@gmail.com>
2022-09-13 08:10:42 +0900

    Initial commit

--- js/texteditor/all513.js
+++ js/texteditor/all513.js
@@ -356,7 +356,7 @@ function fix_proc_updatecodemirror(){
 	}
 }
 fix_codemirror_domReady(function(){
-	var css = '.CodeMirror {line-height: normal !important;} ', head = document.head || document.getElementsByTagName('head')[0], style = document.createElement('style');
+	var css='.CodeMirror{line-height:normal !important;}.CodeMirror-scroll{overflow-x:hidden !important;padding-bottom:13px !important;}', head = document.head || document.getElementsByTagName('head')[0], style = document.createElement('style');	
 	if(head){
 		head.appendChild(style);
 		style.type = 'text/css';
